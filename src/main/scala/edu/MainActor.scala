eb4c39eeebaf3ac1366bd6d783ed94bd0147cdc2
bartlomiej.kalinka <bartlomiej.kalinka@javeo.eu>
2016-11-02 14:05:12 +0100

    logging in actor

--- src/main/scala/edu/MainActor.scala
+++ src/main/scala/edu/MainActor.scala
@@ -1,6 +1,8 @@
 package edu
 
+import akka.actor.ActorLogging
 import akka.persistence.PersistentActor
+import scala.concurrent.duration._
 
 object MainActor {
   case class State(aggr1: String, aggr2: Long) {
@@ -11,10 +13,15 @@ object MainActor {
   }
 }
 
-class MainActor extends PersistentActor {
+class MainActor extends PersistentActor with ActorLogging {
   import MainActor._
+  import context.dispatcher
 
   var state: State = State("", 0L)
+
+  val logScheduler = context.system.scheduler.schedule(5.seconds, 5.seconds) {
+    log.info(s"$persistenceId: state: $state")
+  }
 
   private def persistEvent(e: Event) = persist(e) { event =>
     state = state.update(event)
@@ -22,13 +29,17 @@ class MainActor extends PersistentActor {
 
   def receiveCommand: Receive = {
     case c: Command1 =>
+      log.info(s"received command $c")
       persistEvent(Event1(c.field1))
+      log.info(s"persisted event ${Event1(c.field1)}")
       persistEvent(Event2(c.field2))
+      log.info(s"persisted event ${Event2(c.field2)}")
   }
 
   def receiveRecover: Receive = {
     case e: Event =>
       state = state.update(e)
+      log.info(s"recovered event $e")
   }
 
   def persistenceId: String = s"main-actor-${self.path.name}"
