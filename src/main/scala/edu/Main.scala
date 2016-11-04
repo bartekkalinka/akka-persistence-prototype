086780d546c91729b93301d735e9f02691cb29f3
bartlomiej.kalinka <bartkali@hotmail.com>
2016-11-04 14:43:49 +0100

    config file according to cmd line argument

--- src/main/scala/edu/Main.scala
+++ src/main/scala/edu/Main.scala
@@ -7,11 +7,18 @@ import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
 object Main extends App with Api {
-  override implicit val system = ActorSystem()
+  val configFile = args(0) match {
+      case "java" =>
+        ConfigFactory.load("java-serialization")
+      case "avro" =>
+        ConfigFactory.load("avro-serialization")
+    }
+
+  override val config = Config(configFile)
+  override implicit val system = ActorSystem(name = "MySystem", config = Some(configFile))
   override implicit val executor = system.dispatcher
   implicit val materializer = ActorMaterializer()
 
-  override val config = Config(ConfigFactory.load())
   override val logger = Logging(system, getClass)
   override val mainActor = system.actorOf(Props(new MainActor))
 
